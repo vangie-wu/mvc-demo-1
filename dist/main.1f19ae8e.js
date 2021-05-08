8de1c36b553f12a6b015f90a0a8aa32f1ebf8f26
vangie <wjidian@gmail.com>
2021-05-08 11:26:51 +0800

    second version

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -11436,7 +11436,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62245" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59980" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
